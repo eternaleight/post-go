92cd87c771845f0ef03d2659af08c038f4efa88e
eternaleight <eternaleight0@gmail.com>
2022-07-01 16:49:00 +0900

    fix

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"html/template"
-	"log"
-	"net/http"
-	"os"
-	"os/exec"
+  "html/template"
+  "log"
+  "net/http"
+  "os"
+  "os/exec"
 )
 
 type dogdogdog int
 
 func (m dogdogdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
-	if err != nil {
-		log.Fatalln(err)
-	}
+  err := req.ParseForm()
+  if err != nil {
+    log.Fatalln(err)
+  }
 
-	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+  tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
   log.Println(req.Form)
 }
 
@@ -27,11 +27,11 @@ func init() {
 }
 
 func main() {
- _ = exec.Command("open", "http://localhost:8080").Start()
-	var d dogdogdog
+  _ = exec.Command("open", "http://localhost:8080").Start()
+  var d dogdogdog
   port := os.Getenv("PORT")
-        if port == "" {
-                port = "8080"
-        }
-        http.ListenAndServe(":"+port, d)
+  if port == "" {
+    port = "8080"
+  }
+  http.ListenAndServe(":"+port, d)
 }
